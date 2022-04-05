19c6f0a4ebd7ab5c4ced9ebc00b05e6acae5b6e4
FelipePereiraO <fellipeum@hotmail.com>
2022-04-04 21:55:17 -0300

    Projeto Android

---
+++ app/src/main/java/com/example/academy/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.academy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        Intent it = new Intent(this, login.class);
+        startActivity(it);
+    }
+}
---
+++ app/src/main/java/com/example/academy/login.java
@@ -0,0 +1,26 @@
+package com.example.academy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class login extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private TextView result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        result = findViewById(R.id.result);
+    }
+
+    public void logar(View view){
+        result.setText(String.valueOf(username.getText().toString()));
+    }
+}
