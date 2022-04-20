5dd75e6438d8f575b9ba46b16bd1cd49e8788395
FelipePereiraO <fellipeum@hotmail.com>
2022-04-19 22:14:28 -0300

    Salvando nome do usuario

---
+++ app/src/main/java/com/example/academy/Home.java
@@ -0,0 +1,33 @@
+package com.example.academy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Home extends AppCompatActivity {
+    private SharedPreferences sharedPrefs;
+    public TextView name;
+    public String result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.home);
+        name = findViewById(R.id.nameH);
+        sharedPrefs = getSharedPreferences("dados", MODE_PRIVATE);
+        result = sharedPrefs.getString("name", "");
+
+        name.setText(result);
+    }
+
+    public void onButtonClicked(View view) {
+
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+
+    }
+}
--- app/src/main/java/com/example/academy/Register.java
+++ app/src/main/java/com/example/academy/Register.java
@@ -2,14 +2,56 @@ package com.example.academy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class Register extends AppCompatActivity {
+    private EditText name;
+    private EditText date;
+    private EditText height;
+    private EditText weight;
+    private RadioGroup sex;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.register);
+        name = findViewById(R.id.nameH);
+        date = findViewById(R.id.date);
+        height = findViewById(R.id.height);
+        weight = findViewById(R.id.weight);
+        sex = findViewById(R.id.sex);
+        Button salvar = (Button) findViewById(R.id.save);
+        salvar.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View view){
+                        SharedPreferences sharedPrefs = getSharedPreferences("dados", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor sharedPrefsEditor = sharedPrefs.edit();
+
+                        sharedPrefsEditor.putString("name", name.getText().toString());
+                        //sharedPrefsEditor.putString("Date", StString.valueOf(date));
+                        //sharedPrefsEditor.putFloat("height", Float.parseFloat(height.getText().toString()));
+                        //sharedPrefsEditor.putFloat("weight", Float.parseFloat(String.valueOf(weight.getText().toString())));
+                        //sharedPrefsEditor.putString("Sex", String.valueOf(sex));
+                        sharedPrefsEditor.apply();
+            }
+        });
+
+
+
+
+    }
+    protected void onButtonClicked(View view){
+
+
     }
 }
